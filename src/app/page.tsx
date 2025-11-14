1133ee1e1b4e54ea5caafb485cb93d8eab7eb503
= <=>
2025-11-14 02:16:15 -0500

    Add Prisma test schema

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 
-export default function Home() {
+const Page = async () => {
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center">
       <Button variant="outline">Click</Button>
     </div>
   );
-}
+};
+
+export default Page;
