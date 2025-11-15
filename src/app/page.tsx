e20244496b920b4b235003852937ea353d5480df
= <=>
2025-11-15 16:26:50 -0500

    Add Ingest integration for Bg jobs, Add mproc for easy startup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,32 @@
-import { LogoutButton } from "@/features/auth/components/LogoutButton";
+"use client";
+
+import { Button } from "@/components/ui/button";
 import { requireAuth } from "@/lib/auth-utils";
+import { useTRPC } from "@/trpc/client";
 import { caller } from "@/trpc/server";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
 
-const Page = async () => {
-  await requireAuth();
+const Page = () => {
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+  const { data } = useQuery(trpc.getWorkflows.queryOptions());
 
-  const data = await caller.getUsers();
+  const create = useMutation(
+    trpc.createWorkflow.mutationOptions({
+      onSuccess: () => {
+        toast.success("Job queued");
+      },
+    })
+  );
 
   return (
-    <div className="min-h-screen min-w-screen flex items-center justify-center">
-      <p>protected</p>
+    <div className="min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6">
+      <div>protected</div>
       <div>{JSON.stringify(data)}</div>
+      <Button disabled={create.isPending} onClick={() => create.mutate()}>
+        Create Workflow
+      </Button>
       <div>{/* <LogoutButton /> */}</div>
     </div>
   );
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,15 +1,21 @@
-import { baseProcedure, createTRPCRouter, protectedProcedure } from "../init";
+import { inngest } from "@/inngest/client";
+import { createTRPCRouter, protectedProcedure } from "../init";
 import prisma from "@/lib/db";
 
 export const appRouter = createTRPCRouter({
-  getUsers: protectedProcedure.query(({ ctx }) => {
+  getWorkflows: protectedProcedure.query(({ ctx }) => {
     console.log("getUsers/", { userId: ctx.auth.user.id });
-
-    return prisma.user.findMany({
-      where: {
-        id: ctx.auth.user.id,
+    return prisma.workflow.findMany();
+  }),
+  createWorkflow: protectedProcedure.mutation(async () => {
+    await inngest.send({
+      name: "test/hello.world", // must match function name
+      data: {
+        email: "tests",
       },
     });
+
+    return { success: true, message: "Job queued" };
   }),
 });
 
