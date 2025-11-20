9ad979cfefff13490fe047e2f683f9ebbc7519ad
= <=>
2025-11-20 11:58:56 -0500

    Fix BaseTriggerNode linter complaints

--- src/features/triggers/components/manual-trigger/node.tsx
+++ src/features/triggers/components/manual-trigger/node.tsx
@@ -25,7 +25,8 @@ export const ManualTriggerNode = memo((props: NodeProps) => {
       <BaseTriggerNode
         {...props}
         icon={MousePointerIcon}
-        name="When clicking 'Execute workflow'"
+        name="Manual Trigger"
+        description="This workflow is only triggered on demand"
         status={nodeStatus}
         onSettings={handleOpenSettings}
         onDoubleClick={handleOpenSettings}
