1ecebdb3df0de0d24e065f2a6352103b7076573c
= <=>
2025-11-21 18:24:57 -0500

    Prod updates

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -16,7 +16,7 @@ import { slackChannel } from "@/inngest/channels/slack";
 export const executeWorkflow = inngest.createFunction(
   {
     id: "execute-workflow",
-    retries: 0, // TODO for testing, remove
+    retries: process.env.NODE_ENV === "production" ? 3 : 0,
     onFailure: async ({ event, step }) => {
       return prisma.execution.update({
         where: { inngestEventId: event.data.event.id },
