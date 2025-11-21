aa0d9ffba4fafcbdfcb2233074fb0c4f21334cfa
= <=>
2025-11-20 22:15:50 -0500

    Harden AI credential fetch & Add missing error broadcast

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -56,6 +56,17 @@ export const executeWorkflow = inngest.createFunction(
       return topologicalSort(workflow.nodes, workflow.connections);
     }); // nodes
 
+    // get the userId of the wf owner
+    const userId = await step.run("find-user-id", async () => {
+      const workflow = await prisma.workflow.findUniqueOrThrow({
+        where: { id: workflowId },
+        select: {
+          userId: true,
+        },
+      });
+      return workflow.userId;
+    });
+
     // initialize the context with any initial data from the trigger
     let context = event.data.initialData || {};
 
@@ -67,6 +78,7 @@ export const executeWorkflow = inngest.createFunction(
       context = await executor({
         data: node.data as Record<string, unknown>,
         nodeId: node.id,
+        userId,
         context,
         step,
         publish,
